cdcd730683019a45267276622cb835a3dd1688d8
Gamepro5 <gamepro543@gmail.com>
2023-02-05 15:44:48 -0800

    GDB

--- Lab_1/averages.c
+++ Lab_1/averages.c
@@ -3,15 +3,29 @@
 int main( int argc, char *argv[] )  {
 
     int input_index=1;
-    float inpt, inputsum;
+    float inpt, oddinputsum, eveninputsum, oddinputcount, eveninputcount;
 
     printf("Enter the 1st number: ");
     scanf("%f", &inpt);
     fflush(stdout);
     if (inpt != 0) {
-        inputsum += inpt;
-        input_index += 1;
-    }
+            int tempsum = 0;
+            int remaining_input = inpt;
+            while (remaining_input != 0) {
+                int temp = remaining_input % 10;
+                remaining_input = (remaining_input - temp)/10;
+                tempsum += temp;
+            }
+            printf("%i\n",tempsum);
+            if (tempsum % 2 == 0) {
+                eveninputsum += inpt;
+                eveninputcount += 1;
+            } else {
+                oddinputsum += inpt;
+                oddinputcount += 1;
+            }
+            input_index += 1;
+        };
     while (inpt != 0) {
         inpt = 0;
         if (input_index == 2) {
@@ -24,20 +38,38 @@ int main( int argc, char *argv[] )  {
         scanf("%f", &inpt);
         fflush(stdout);
         if (inpt != 0) {
-            inputsum += inpt;
+            int tempsum = 0;
+            int remaining_input = inpt;
+            while (remaining_input != 0) {
+                int temp = remaining_input % 10;
+                remaining_input = (remaining_input - temp)/10;
+                tempsum += temp;
+            }
+            printf("%i\n",tempsum);
+            if (tempsum % 2 == 0) {
+                eveninputsum += inpt;
+                eveninputcount += 1;
+            } else {
+                oddinputsum += inpt;
+                oddinputcount += 1;
+            }
             input_index += 1;
         };
     };
     input_index -= 1;
-    if (inputsum == 0) {
+    if (eveninputsum == 0 && oddinputsum == 0) {
         printf("There is no average to compute.\n");
     } else {
-        float result = (inputsum / (float) input_index);
-        if ((int)inputsum % 2 == 0) { //is even
-            printf("Average of inputs whose digits sum up to an even number: %i\n", (int)result);
-        } else { // is odd
-            printf("Average of inputs whose digits sum up to an odd number: %f\n", (float)result);
+        if (eveninputsum != 0) {
+            float result = (eveninputsum / (float) eveninputcount);
+            printf("Average of inputs whose digits sum up to an even number: %f\n", result);
         }
+        if (oddinputsum != 0) {
+            float result = (oddinputsum / (float) oddinputcount);
+            printf("Average of inputs whose digits sum up to an odd number: %f\n", result);
+        }
+       
     }
+    
     return 0; 
 } 
