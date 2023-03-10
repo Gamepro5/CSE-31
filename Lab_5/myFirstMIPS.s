757dce0e384fd8175ceabc836b0359968a4f6514
Gamepro5 <gamepro543@gmail.com>
2023-03-10 14:14:06 -0800

    Update myFirstMIPS.s

--- Lab_5/myFirstMIPS.s
+++ Lab_5/myFirstMIPS.s
@@ -1,11 +1,11 @@
-main: 	addi     $t0, $0, 50
+main: 	addi     $t0, $s0, 0
 	addi	$t1, $t0, -7
 	add	$t2, $t1, $t0
 	addi	$t3, $t2, 2
 	add	$t4, $t3, $t2
 	addi	$t5, $t4, -28
 	sub	$t6, $t4, $t5
-	addi	$t7, $t6, 1
+	addi	$t7, $t6, 12
 	j finish
 	
 
