8d4dec361dc50835bf66770b8c066e541529c3b5
Gamepro5 <gamepro543@gmail.com>
2023-04-02 01:04:02 -0700

    lab 7

---
+++ Lab_7/proc1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int sub(int a) {
+    int b = 2;
+    return a - b;
+}
+
+int sum(int n) {
+    int m = 15;
+    return n + sub(m + n);
+}
+
+int main() {
+    int m = 10;
+    int n = 5;
+    
+
+    y = y + x + sum(x);
+
+    printf("%d\n", y);
+    return 0;
+}
